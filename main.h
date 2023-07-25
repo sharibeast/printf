ba6b709fefb0cb757fd1632d35576b1e203fa448
Almas <almasemilius@gmail.com>
2023-07-26 02:34:00 +0300

    adding flags

--- main.h
+++ main.h
@@ -6,7 +6,11 @@
 
 #define BUFF_SIZE 1024
 
-
+#define FLAG_MINUS 1
+#define FLAG_PLUS 2
+#define FLAG_ZERO 4
+#define FLAG_HASH 8
+#define FLAG_SPACE 16
 int _printf(const char *format, ...);
 
 #endif
