fa55651c466d989a6c90e92c3806d3af80d4a8d7
Almas <almasemilius@gmail.com>
2023-07-26 00:06:58 +0300

    fixing switch case

--- _printf.c
+++ _printf.c
@@ -5,37 +5,52 @@
 int _printf(const char *format, ...)
 {
         va_list args;
-	va_start(args, format);
+        va_start(args, format);
 
-	int char_count;
-	char_count = 0;
+        int char_count;
+        char_count = 0;
 
-	while (*format != '\0')
-	{
-		if (*format == '%')
-		{
-			format++;
+        while (*format != '\0')
+        {
+                if (*format == '%')
+                {
+                        format++;
 
-			switch (*format)
-			{
-			case 'c':
-			{
-				int c = va_arg(args, int);
-				putchar(c);
-				char_count++;
-				break;
-			}
-			case '%':
-				putchar('%');
-				char_count++;
-				break;
-		}
-		else
-		{
-			putchar(*format);
-			char_count++;
-		}
-		format++;
-	}
-
+                        switch (*format)
+                        {
+                        case 'c':
+                        {
+                                int c = va_arg(args, int);
+                                putchar(c);
+                                char_count++;
+                                break;
+                        }
+                        case '%':
+                                putchar('%');
+                                char_count++;
+                                break;
+                        case 's':
+                        {
+                                char *str = va_arg(args, char *);
+                                while (*str != '\0')
+                                {
+                                        putchar(*str);
+                                        str++;
+                                        char_count++;
+                                }
+                                break;
+                        }
+                        default:
+                                putchar('%');
+                                putchar(*format);
+                                char_count += 2;
+                        }
+                }
+                else
+                {
+                        putchar(*format);
+                        char_count++;
+                }
+                format++;
+        }
 }
